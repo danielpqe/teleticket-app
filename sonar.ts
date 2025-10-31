03ee92b4726b3e2ba002f1d28fa34e083bbcde49
Daniel Quispe <danielpqe@gmail.com>
2025-10-31 13:39:16 -0500

    Updated postgres container

--- sonar.ts
+++ sonar.ts
@@ -4,9 +4,9 @@ scanner(
   {
     serverUrl: 'http://localhost:9000',
     options: {
-      'sonar.token': 'sqp_ac78d125c8bd2045411337275928be6fa5a8d062',
-      'sonar.projectKey': 'ticketlab-app',
-      'sonar.projectName': 'TicketLab App',
+      'sonar.token': 'sqp_f487cf4b1dc8c745050685e2dc4f6989390862e7',
+      'sonar.projectKey': 'teleticket-app',
+      'sonar.projectName': 'Teleticket App',
       'sonar.projectVersion': '1.0',
 
       'sonar.sources': 'src',
